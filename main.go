44d8e823a06f2c20321cd2ce5c9f26bf181291f4
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-02-17 23:28:53 +0300

    add builtin command 'type'

--- main.go
+++ main.go
@@ -4,8 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
+
+type Command func(args []string) error
+
+var builtins map[string]Command
+
+func init() {
+	builtins = map[string]Command{
+		"type": handleType,
+	}
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -22,10 +33,31 @@ func main() {
 		}
 
 		command := words[0]
+		args := words[1:]
 
-		switch command {
-		default:
+		cmdFunc, found := builtins[command]
+		if found {
+			cmdFunc(args)
+		} else {
 			fmt.Printf("%s: command not found\n", command)
+			continue
 		}
 	}
+}
+
+func handleType(args []string) error {
+	cmd := args[0]
+	_, found := builtins[cmd]
+	if found {
+		fmt.Printf("%s is a shell builtin\n", cmd)
+	} else {
+		filePath, err := exec.LookPath(cmd)
+		if err != nil {
+			fmt.Printf("%s: not found\n", cmd)
+			return nil
+		}
+
+		fmt.Printf("%s is %s\n", cmd, filePath)
+	}
+	return nil
 }
