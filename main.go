702a40ab1b48bfc6812f00eea8ad1ccd5a503afd
Fatih <122681611+yildiz-fatih@users.noreply.github.com>
2026-02-17 13:00:23 +0300

    add REPL (read-eval-print loop)

--- main.go
+++ main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("$ ")
+
+		scanner.Scan()
+		input := scanner.Text()
+
+		words := strings.Fields(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+
+		switch command {
+		default:
+			fmt.Printf("%s: command not found\n", command)
+		}
+	}
 }
